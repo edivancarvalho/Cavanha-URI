e5f02d912d43a86980f9033ea1f2bbc2c57defa1
CavanhaMan <cavanhaman@yahoo.com.br>
2018-04-27 01:48:47 -0300

    Add files via upload

--- URI_2621 Quantidades entre 10 e 20.sql
+++ URI_2621 Quantidades entre 10 e 20.sql
@@ -11,8 +11,3 @@ select products.name
  from products inner join providers on products.id_providers = providers.id 
  where providers.name like 'P%'
  and products.amount between 10 and 20;
-
-select products.name
- from products inner join providers on products.id_providers = providers.id
- where providers.name='P%'
- and (products.amount between 10 and 20);
