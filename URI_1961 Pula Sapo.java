77d1c2610cced021d2810132ba73e52ed7fd65b7
CavanhaMan <cavanhaman@hotmail.com>
2018-07-12 13:55:23 -0300

    Add files via upload

--- URI_1961 Pula Sapo.java
+++ URI_1961 Pula Sapo.java
@@ -2,7 +2,7 @@
 +------------------+
 |Rodrigo CavanhaMan|
 |     URI 1961     |
-|     Pula Sapo    |
+|    Pula  Sapo    |
 +------------------+
 */
 import java.util.Scanner;
@@ -16,16 +16,27 @@ public class Main {
 		int ncanos = sc.nextInt();
 		int canoant, canoatual=0;
 		int ok=0;
-		canoant=sc.nextInt();
+		canoant=sc.nextInt();				//entra primeiro cano
 		for (int i=1 ; i<ncanos ; i++){
-			canoatual = sc.nextInt();
-			if (canoant+sapo < canoatual){
-				System.out.println("GAME OVER");
-				ok=1;
-				break;
+			canoatual = sc.nextInt();		//entra segundo cano em diante
+			if (canoant < canoatual){
+				if (canoant+sapo < canoatual){	//compara o canto anterior com o atual
+					System.out.println("GAME OVER"); //se for menor, game over
+					ok=1;
+					break;
+				}
+				else
+					canoant=canoatual;			//senao, cano anterior passa a ser o atual
 			}
-			else
-				canoant=canoatual;
+			else{
+				if (canoatual+sapo < canoant){
+					System.out.println("GAME OVER");
+					ok=1;
+					break;
+				}
+				else
+					canoant=canoatual;
+			}
 		}
 		if(ok==0)
 			System.out.println("YOU WIN");
